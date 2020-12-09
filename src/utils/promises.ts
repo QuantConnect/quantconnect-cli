b244fb58512c69ca1ae6f791b6555220d8cb442d
Jasper van Merle <jaspervmerle@gmail.com>
2020-12-09 18:03:29 +0100

    Fix bugs in polling mechanism and remove retrying failed requests

--- src/utils/promises.ts
+++ src/utils/promises.ts
@@ -15,18 +15,13 @@ interface PollOptions<T> {
 
 /**
  * When running certain tasks we need to continuously call the API to retrieve
- * new information and to see if the task is completed. This process can be
- * kinda flaky, so to make it more robust we:
- * 1. Retry failing requests up to 5 times to make sure a few sporadic network
- *    errors don't cause the entire task to fail.
- * 2. Poll at varying intervals, starting from 250ms up to 10,000ms, to reduce
- *    the amount of requests being made while still delivering results swiftly
- *    if the task is done quickly.
+ * new information and to see if the task is completed. To prevent making
+ * thousands of requests when waiting for long-running tasks to complete we
+ * poll at varying intervals, starting from 250ms up to 10,000ms. This reduces
+ * the amount of requests being made while still delivering results swiftly if
+ * the task is done quickly.
  */
 export async function poll<T>({ makeRequest, isDone, shouldIgnoreError, getProgress }: PollOptions<T>): Promise<T> {
-  let retryCounter = 0;
-  const maxRetries = 5;
-
   // [<interval>, <amount of times to use the interval>]
   // The comments below assume the API call is instant to make the chosen intervals more clear
   const intervals: [number, number][] = [
@@ -49,34 +44,27 @@ export async function poll<T>({ makeRequest, isDone, shouldIgnoreError, getProgr
   const progressBar = getProgress !== undefined ? logger.progress() : null;
 
   while (true) {
-    let skipIsDone = false;
+    let dataUpdated = true;
 
     try {
       data = await makeRequest();
-      retryCounter = 0;
     } catch (err) {
       if (shouldIgnoreError === undefined || !shouldIgnoreError(err)) {
-        retryCounter++;
-
-        logger.debug(`Request failed while polling for new information (attempt ${retryCounter}/${maxRetries})`);
-
-        if (retryCounter === maxRetries) {
-          if (progressBar !== null) {
-            progressBar.terminate();
-          }
-
-          throw err;
-        }
+        throw err;
       }
 
-      skipIsDone = true;
+      dataUpdated = false;
     }
 
-    if (progressBar !== null) {
+    if (dataUpdated && progressBar !== null && !progressBar.complete) {
       progressBar.update(getProgress(data));
     }
 
-    if (!skipIsDone && isDone(data)) {
+    if (dataUpdated && isDone(data)) {
+      if (progressBar !== null && !progressBar.complete) {
+        progressBar.update(1.0);
+      }
+
       return data;
     }
 
